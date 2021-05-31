5caf14c69e724ef972e8bdcc95860c3efa75c8c6
Brandon Defoe <defoe.brand@gmail.com>
2021-05-31 18:14:51 +0300

    Create / destroy sessions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,12 @@ require("semantic-ui-sass")
 
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
+    $('.message .close')
+        .on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade')
+            ;
+        })
+    ;
 })
