9e158bdd1eb12d90086d630e23540b323a976e8c
Brandon Defoe <defoe.brand@gmail.com>
2021-05-31 22:06:44 +0300

    Add enter key input function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,16 +14,26 @@ ActiveStorage.start()
 
 require("semantic-ui-sass")
 
-export const scroll_bottom = function() {
+export const scroll_bottom = () => {
     if($('#messages').length > 0) {
         $('#messages').scrollTop($('#messages')[0].scrollHeight)
-    }
-}
+    };
+};
+
+const submit_message = () => {
+    $('#message_body').on('keydown', (e) => {
+        if(e.keyCode === 13){
+            $('button').click();
+            e.target.value = '';
+        };
+    })
+};
 
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
-    scroll_bottom()
+    submit_message();
+    scroll_bottom();
 })
